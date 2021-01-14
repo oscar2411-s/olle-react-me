72ab5150565517c507193057303432efec28c6d5
Olle <oscar.gaardsted.spalk@gmail.com>
2021-01-14 23:50:42 +0100

    Update cards

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
+import logo from './olle.svg';
 import './App.css';
 import Section1 from './Components/Section1/Section1';
+import Section2 from './Components/Section2/Section2';
 
 function App() {
   return (
     <div className="App">
       <Section1></Section1>
+      <Section2></Section2>
     </div>
   );
 }
--- src/Components/Section2/Section2.js
+++ src/Components/Section2/Section2.js
@@ -1,20 +1,53 @@
 import React from 'react';
-import './Section1.css';
+import './Section2.css';
 import {ReactComponent as PilNed} from '../../Assets/Icons/down-arrow.svg';
 
-class Section1 extends React.Component{
+class Section2 extends React.Component{
     render(){
         return(
             <div className='section2'>
                 <div className='cardbox'>
+                    <a href="#" className="touch-wrap">
                     <div className='card'>
-                        <h3 className='cardtitle'></h3>
-                        <p className='cardcontent'></p>
+                        <h3 className='cardtitle'>Lorem Ipsum</h3>
+                        <p className='cardcontent'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                     </div>
+                    </a>
+                    <a href="#" className="touch-wrap">
+                    <div className='card'>
+                        <h3 className='cardtitle'>Lorem Ipsum</h3>
+                        <p className='cardcontent'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    </div>
+                    </a>
+                    <a href="#" className="touch-wrap">
+                    <div className='card'>
+                        <h3 className='cardtitle'>Lorem Ipsum</h3>
+                        <p className='cardcontent'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    </div>
+                    </a>
+                    <a href="#" className="touch-wrap">
+                    <div className='card'>
+                        <h3 className='cardtitle'>Lorem Ipsum</h3>
+                        <p className='cardcontent'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    </div>
+                    </a>
+                    <a href="#" className="touch-wrap">
+                    <div className='card'>
+                        <h3 className='cardtitle'>Lorem Ipsum</h3>
+                        <p className='cardcontent'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    </div>
+                    </a>
+                    <a href="#" className="touch-wrap">
+                    <div className='card'>
+                        <h3 className='cardtitle'>Lorem Ipsum</h3>
+                        <p className='cardcontent'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    </div>
+                    </a>
+                    
                 </div>
             </div>
         );
     }
 }
 
-export default Section1;
+export default Section2;
